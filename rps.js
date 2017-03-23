42a570c9ccb148154daa3511308160b751c1494a
Nicole Watkins <nicole.watkins@detroitlabs.com>
2017-03-23 14:33:39 -0400

    Adding win/lose outcome explanation messages

---
+++ rps.js
@@ -0,0 +1,21 @@
+// user must make a selection
+// computer must create random responses
+// user should be notified of outcome and given opportunity to play again
+// user should be told how they won/lost
+
+
+
+
+
+var outcomeMessages = {
+	scissorspaper: "Scissors cuts paper!", 
+	paperRock: "Paper covers rock!",
+	rockLizard: "Rock crushes lizard!",
+	lizardSpock: "Lizard poisons Spock!", 
+	spockScissors: "Spock smashes scissors!", 
+	scissorsLizard: "Scissors decapitates lizard!",
+	lizardPaper: "Lizard eats paper!",
+	paperSpock: "Paper disproves Spock!",
+	spockRock: "Spock vaporizes rock!",
+	rockScissors: "Rock crushes scissors!"
+};
