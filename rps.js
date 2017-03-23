8c561323e6cd0f6d0d03089fcc52598c249fcd14
Nicole Watkins <nicole.watkins@detroitlabs.com>
2017-03-23 15:20:12 -0400

    Adding menu logic, menu prompts, print menu, and declared corresponding variables

--- rps.js
+++ rps.js
@@ -22,6 +22,51 @@ var wipeScreen = function () {
 
 
 
+ var printMenu = function() {
+    for (var key in mainMenuPrompts) {
+      console.log(mainMenuPrompts[key]);
+    }
+};
+
+
+
+var mainMenuPrompts = { 
+                        welcome:            "**************************************************************************\n" + 
+                                            "*         Welcome to Rock, Paper, Scissors, Lizard, Spock!!!!            *\n" +
+                                            "**************************************************************************\n"  
+                                 ,
+                                 
+                        play: "Enter 'Play' or '1' to play a new round",
+                        score: "Enter 'Score' or '2' to see current score ",                    
+                        exit: "Enter 'Exit' or '3' to exit the game\n"
+                      };
+
+ var userMessages = {
+ 	handShape: "Enter your choice: Rock, Paper, Scissors, Lizard, Spock",
+ 	record: "Your current score is:",
+	badInput: "Please make a valid selection.",
+	exit: "You've logged out of the game. See ya!"
+};
+
+
+var mainMenu = function() {
+  wipeScreen();
+  sleep(400);
+  printMenu();
+  sleep(400);
+    var userSelection = sget("What would you like to do?").trim();
+    
+    if(userSelection.toLowerCase() == "play" || userSelection == 1) {
+        initializeGame();
+    } else if(userSelection.toLowerCase() == "score" || userSelection == 2) {
+        viewScore();
+    } else if(userSelection.toLowerCase() == "exit" || userSelection == 3) {
+        exit();
+    } else {
+      console.log(userMessages.badInput);
+      mainMenu();
+	}
+};
 
 
 var outcomeMessages = {
@@ -36,4 +81,20 @@ var outcomeMessages = {
 	spockRock: "Spock vaporizes rock!",
 	rockScissors: "Rock crushes scissors!"
 };
+
+
+var initializeGame = function () {};
+var viewScore = function () {};
+
+
+
+var exit = function() {
+  console.log(userMessages.exit);
+  sleep(600);
+};
+
+run = function() {
+    mainMenu();
+   
+}();
 
